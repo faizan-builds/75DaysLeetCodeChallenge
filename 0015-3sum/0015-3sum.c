106f6d9667677948915d285901cfb6e73a25f36a
faizan-builds <245393784+faizan-builds@users.noreply.github.com>
2026-03-23 17:33:17 +0530

    Time: 55 ms (18.91%), Space: 36.6 MB (68.59%) - LeetHub

---
+++ 0015-3sum/0015-3sum.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes) {
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    int capacity = 100;
+    int **result = malloc(sizeof(int*) * capacity);
+    *returnColumnSizes = malloc(sizeof(int) * capacity);
+    *returnSize = 0;
+
+    for (int i = 0; i < numsSize - 2; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+        int left = i + 1, right = numsSize - 1;
+
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if (sum == 0) {
+
+                // 🔥 Resize BEFORE inserting
+                if (*returnSize >= capacity) {
+                    capacity *= 2;
+                    result = realloc(result, sizeof(int*) * capacity);
+                    *returnColumnSizes = realloc(*returnColumnSizes, sizeof(int) * capacity);
+                }
+
+                result[*returnSize] = malloc(sizeof(int) * 3);
+                result[*returnSize][0] = nums[i];
+                result[*returnSize][1] = nums[left];
+                result[*returnSize][2] = nums[right];
+
+                (*returnColumnSizes)[*returnSize] = 3;
+                (*returnSize)++;
+
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+
+                left++;
+                right--;
+            }
+            else if (sum < 0) left++;
+            else right--;
+        }
+    }
+
+    return result;
+}
